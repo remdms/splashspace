8e19b05c97c5892a33082c51c33fa2920a4a1bfd
Remi Dumas <remipdumas@gmail.com>
2024-08-13 11:55:12 +0200

    added cloudinary and activestorage gems. Added has_one_attached :photo to Pool

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -4,8 +4,9 @@ class Pool < ApplicationRecord
   # ligne suivante à decommenter si nécessaire
   # has_many :users, through: :bookings
 
-  # on lie une picture Active Storage
-  # has_one_attached :picture
+  # on lie une photo Active Storage (ajout Remi)
+  has_one_attached :photo
+  
   validates :address, presence: true
   validates :name, presence: true
   validates :price_per_day, presence: true
