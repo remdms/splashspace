0ebd67113374a1fd1b30baf912403a2ce0b7f10c
alexandra david <alexdav13@gmail.com>
2024-08-13 16:40:52 +0200

    seed cloudinary 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,20 @@ puts 'piscines détruits'
 User.destroy_all
 puts 'users bien détruits'
 
+users = []
 #   end
 puts "creating user"
 user1 = User.create!(first_name: "Paul", last_name: "Pivet", pseudo: "PP7", email: "exemple1@gmail.com", password: "exemple1")
+users << user1
 
 user2 = User.create!(first_name: "Jeanne", last_name: "Boulanger", pseudo: "Jeanne.B", email: "exemple2@gmail.com", password: "exemple2")
+users << user2
 
 user3 = User.create!(first_name: "Marc", last_name: "Dupont", pseudo: "MarcDc", email: "exemple3@gmail.com", password: "exemple3")
+users << user3
 
 user4 = User.create!(first_name: "Sophie", last_name: "Leroy", pseudo: "SophieL", email: "exemple4@gmail.com", password: "exemple4")
+users << user4
 
 # user5 = User.create!(first_name: "Pierre", last_name: "Martin", pseudo: "PierreM", email: "exemple1@gmail.com", password: "exemple1")
 
@@ -40,6 +45,7 @@ user4 = User.create!(first_name: "Sophie", last_name: "Leroy", pseudo: "SophieL"
 # user12 = User.create!(first_name: "Camille", last_name: "Garcia", pseudo: "CamilleG", email: "garcia@gmail.com", password: "123456")
 p User.all
 
+
 puts "creating pool"
 pool1 = Pool.new(
   name: "Crystal Lagoon Oasis",
@@ -47,7 +53,7 @@ pool1 = Pool.new(
   pool_type: "Salt",
   address: "30 rue voisin, 92600 Asnières, France",
   price_per_day: 120,
-  user_id: user1.id
+  user_id: users.sample.id
 )
 file = URI.open("https://res.cloudinary.com/dsxqbci59/image/upload/v1723555107/Crystal_Lagoon_Oasis.jpg")
 
@@ -59,7 +65,7 @@ pool2 = Pool.new(
   overview: "A tranquil, azure-blue pool nestled within a secluded garden", pool_type: "Chlorine",
   address: "12 rue de la Mer, 75008 Paris, France",
   price_per_day: 90,
-  user_id: user2.id
+  user_id: users.sample.id
 )
 file = URI.open("https://res.cloudinary.com/dsxqbci59/image/upload/v1723555191/Le_Bleu_Paradis.jpg")
 
@@ -72,7 +78,7 @@ pool3 = Pool.new(
   pool_type: "Salt",
   address: "8 chemin des Lavandes, 13100 Aix-en-Provence, France",
   price_per_day: 160,
-  user_id: user3.id
+  user_id: users.sample.id
 )
 file = URI.open("https://res.cloudinary.com/dsxqbci59/image/upload/v1723557246/Etang_Emeraude.jpg")
 
@@ -84,7 +90,7 @@ pool4 = Pool.new(
   overview: "An opulent pool with golden mosaics and luxurious cabanas", pool_type: "Chlorine",
   address: "25 avenue des Champs-Élysées, 75008 Paris, France",
   price_per_day: 240,
-  user_id: 4
+  user_id: users.sample.id
 )
 file = URI.open("https://res.cloudinary.com/dsxqbci59/image/upload/v1723557583/Bains_Or.jpg")
 
@@ -97,7 +103,7 @@ pool5 = Pool.new(
   pool_type: "Ozone",
   address: "14 route des Vins, 67000 Strasbourg, France",
   price_per_day: 280,
-  user_id: 1
+  user_id: users.sample.id
 )
 file = URI.open("https://res.cloudinary.com/dsxqbci59/image/upload/v1723557766/Bassin_Saphir.jpg")
 
@@ -110,7 +116,7 @@ pool6 = Pool.new(
   pool_type: "Chlorine",
   address: "45 rue du Château, 33000 Bordeaux, France",
   price_per_day: 120,
-  user_id: 2
+  user_id: users.sample.id
 )
 file = URI.open("https://res.cloudinary.com/dsxqbci59/image/upload/v1723557955/Chateau_Eau.jpg")
 
@@ -123,7 +129,7 @@ pool7 = Pool.new(
   pool_type: "Salt",
   address: "12 avenue de la Croisette, 06400 Cannes, France",
   price_per_day: 200,
-  user_id: 3
+  user_id: users.sample.id
 )
 file = URI.open("https://res.cloudinary.com/dsxqbci59/image/upload/v1723558127/Palais_Bleu.jpg")
 
@@ -136,7 +142,7 @@ pool8 = Pool.new(
   pool_type: "Ozone",
   address: "22 chemin des Lavandes, 84000 Avignon, France",
   price_per_day: 130,
-  user_id: 4
+  user_id: users.sample.id
 )
 file = URI.open("https://res.cloudinary.com/dsxqbci59/image/upload/v1723558382/Villa_Emeraude.jpg")
 
@@ -149,7 +155,7 @@ pool9 = Pool.new(
   pool_type: "Salt",
   address: "10 boulevard de la Croisette, 06000 Nice, France",
   price_per_day: 110,
-  user_id: 2
+  user_id: users.sample.id
 )
 file = URI.open("https://res.cloudinary.com/dsxqbci59/image/upload/v1723558549/Horizon_Azur.jpg")
 
@@ -160,7 +166,7 @@ pool10 = Pool.new(
   name: "Villa de Sol azurée",
   overview: "Perched on the cliffs of Corsica, offering panoramic views of the turquoise waters of the Mediterranean.", pool_type: "Chlorine",
   address: "118 rue de la Mer, 20100 Bonifacio, Corsica, France", price_per_day: 85,
-  user_id: 1
+  user_id: users.sample.id
 )
 file = URI.open("https://res.cloudinary.com/dsxqbci59/image/upload/v1723558720/Villa_Sol_azuree.jpg")
 
@@ -172,7 +178,7 @@ pool11 = Pool.new(
   overview: "An exotic retreat located in the heart of the Camargue, featuring a pool that mimics the natural oasis of the region",
   pool_type: "UV",
   address: "25 route de l’Oasis, 13460 Les Saintes-Maries-de-la-Mer, France", price_per_day: 110,
-  user_id: 1
+  user_id: users.sample.id
 )
 file = URI.open("https://res.cloudinary.com/dsxqbci59/image/upload/v1723558885/Oasis_Desert.jpg")
 
@@ -183,7 +189,7 @@ pool12 = Pool.new(
   name: "Crique Cachée",
   overview: "A secluded paradise surrounded by pine forests and hidden coves, offering a private and intimate experience on the Brittany coast", pool_type: "UV",
   address: "5 rue des Goélands, 29660 Carantec, Brittany, France", price_per_day: 160,
-  user_id: 1
+  user_id: users.sample.id
 )
 file = URI.open("https://res.cloudinary.com/dsxqbci59/image/upload/v1723559042/Crique_Cachee.jpg")
 
