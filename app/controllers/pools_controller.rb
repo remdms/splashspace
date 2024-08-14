deaabd20be9eea7e53e38bd0cf87b9593603fa40
Remi Dumas <remipdumas@gmail.com>
2024-08-14 16:07:04 +0200

    new pool page

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -9,9 +9,27 @@ class PoolsController < ApplicationController
     @booking = Booking.new
   end
 
+  def new
+    @pool = Pool.new
+  end
+
+  def create
+    @pool = Pool.new(pool_params)
+    @pool.user = current_user
+    if @pool.save
+      redirect_to pool_path(@pool)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_pool
     @pool = Pool.find(params[:id])
+  end
+
+  def pool_params
+    params.require(:pool).permit(:name, :address, :area, :pool_type, :price_per_day, :photo)
   end
 end
