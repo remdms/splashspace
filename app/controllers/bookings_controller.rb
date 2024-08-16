2cb6040bc7d153f66a81b415e711f6fbd7c9f22f
alexandra david <alexdav13@gmail.com>
2024-08-16 11:11:07 +0200

    bookings show and alerts

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,7 +17,7 @@ class BookingsController < ApplicationController
     @booking.total_price = (@booking.end_date.day - @booking.start_date.day) * @booking.pool.price_per_day
     if @booking.save
       # TODO change path to user show
-      redirect_to user_path(current_user)
+      redirect_to user_path(current_user), notice: "Your booking has been created"
     else
       render "pools/show", status: :unprocessable_entity
     end
