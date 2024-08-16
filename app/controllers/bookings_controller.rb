91ff67e805614a8cf2ca45c1b5d22aab1246c39f
Remi Dumas <remipdumas@gmail.com>
2024-08-16 11:53:55 +0200

    changed dashboard route to pages#dashboard, updated dashboard and navbar

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,7 +9,7 @@ class BookingsController < ApplicationController
     @booking.total_price = (@booking.end_date.day - @booking.start_date.day) * @booking.pool.price_per_day
     if @booking.save
       # TODO change path to user show
-      redirect_to user_path(current_user)
+      redirect_to dashboard_path
     else
       render "pools/show", status: :unprocessable_entity
     end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    @user = current_user
+    @booking_requests = Booking.where(pool: @user)
+  end
 end
